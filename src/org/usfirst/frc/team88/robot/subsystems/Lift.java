32803f3ca9da15561d5c1af0c8e0702b19a478b0
Nick Avtges <navtges@gmail.com>
2018-01-26 09:49:51 -0500

    Format Lift.java

--- src/org/usfirst/frc/team88/robot/subsystems/Lift.java
+++ src/org/usfirst/frc/team88/robot/subsystems/Lift.java
@@ -13,35 +13,34 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  *
  */
 public class Lift extends Subsystem {
-private static final int SLOTIDX = 0;
-private static final int TIMEOUTMS = 0;
-private static final int FORWARDLIMIT = 0;
-private static final int REVERSELIMIT = 0;
-private TalonSRX liftTalon;
-private VictorSPX rightwing;
-   public Lift(){
-	   liftTalon = new TalonSRX(RobotMap.liftTalon);
-	   rightwing = new VictorSPX (RobotMap.rightwing);
-	   
-	   liftTalon.configSelectedFeedbackSensor(FeedbackDevice.Analog, SLOTIDX, TIMEOUTMS);
-	   liftTalon.configForwardSoftLimitThreshold(FORWARDLIMIT, TIMEOUTMS);
-	   liftTalon.configForwardSoftLimitEnable(true, TIMEOUTMS);
-	   
-	   liftTalon.configReverseSoftLimitThreshold(REVERSELIMIT, TIMEOUTMS);
-	   liftTalon.configReverseSoftLimitEnable(true, TIMEOUTMS);
-	  
+	private static final int SLOTIDX = 0;
+	private static final int TIMEOUTMS = 0;
+	private static final int FORWARDLIMIT = 0;
+	private static final int REVERSELIMIT = 0;
+	private TalonSRX liftTalon;
+	private VictorSPX rightwing;
+
+	public Lift() {
+		liftTalon = new TalonSRX(RobotMap.liftTalon);
+		rightwing = new VictorSPX(RobotMap.rightwing);
+
+		liftTalon.configSelectedFeedbackSensor(FeedbackDevice.Analog, SLOTIDX, TIMEOUTMS);
+		liftTalon.configForwardSoftLimitThreshold(FORWARDLIMIT, TIMEOUTMS);
+		liftTalon.configForwardSoftLimitEnable(true, TIMEOUTMS);
+
+		liftTalon.configReverseSoftLimitThreshold(REVERSELIMIT, TIMEOUTMS);
+		liftTalon.configReverseSoftLimitEnable(true, TIMEOUTMS);
+
 		rightwing.set(ControlMode.Follower, RobotMap.liftTalon);
-		
-   }
 
-   public void levitate(double velocity) {
-	   liftTalon.set(ControlMode.PercentOutput, velocity);
-   }
-   
-   
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
+	}
+
+	public void levitate(double velocity) {
+		liftTalon.set(ControlMode.PercentOutput, velocity);
+	}
+
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
 }
-
