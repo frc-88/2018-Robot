c168d60bec8c1ae9ce6661b91d167605cf4c6f3e
Nick Avtges <navtges@gmail.com>
2018-02-16 21:27:25 -0500

    Add a "soft landing" feature when lowering the lift.

---
+++ src/org/usfirst/frc/team88/robot/commands/LiftSoftLanding.java
@@ -0,0 +1,56 @@
+package org.usfirst.frc.team88.robot.commands;
+
+import org.usfirst.frc.team88.robot.Robot;
+import org.usfirst.frc.team88.robot.subsystems.Lift;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class LiftSoftLanding extends Command {
+
+	private static final int OFFSET = 100;
+	private boolean done;
+	private int count;
+	
+    public LiftSoftLanding() {
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	done = false;
+    	count = 0;
+		Robot.lift.setPosition(Lift.POS_BOTTOM + OFFSET);
+		Robot.lift.gotoPosition();
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    	if (Robot.lift.onTarget(Lift.POS_BOTTOM + OFFSET)) {
+    		count++;
+    	}
+    	
+    	if (count > 10) {
+    		Robot.lift.setPosition(Lift.POS_BOTTOM);
+    		Robot.lift.gotoPosition();
+    		done = true;
+    	}
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return done;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
--- src/org/usfirst/frc/team88/robot/subsystems/Lift.java
+++ src/org/usfirst/frc/team88/robot/subsystems/Lift.java
@@ -34,7 +34,7 @@ public class Lift extends Subsystem {
 
 	private static final int FORWARDLIMIT = 750;
 	private static final int REVERSELIMIT = 80;
-	public final static int POS_BOTTOM = 110;
+	public final static int POS_BOTTOM = 80;
 	public final static int POS_SWITCH = 260;
 	public final static int POS_LOW_SCALE = 520;
 	public final static int POS_MID_SCALE = 600;
