a185ecd03cf5c4a5be238a7475e129f9405430f4
tj2 <navtges@gmail.com>
2018-02-24 16:45:42 -0500

    TripleStack tuning

--- src/org/usfirst/frc/team88/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team88/robot/subsystems/Drive.java
@@ -30,7 +30,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class Drive extends Subsystem implements PIDOutput {
 
 	private final static boolean CLOSED_LOOP = true;
-	private final static boolean SPLIT_ARCADE = false;
+	private final static boolean SPLIT_ARCADE = true;
 
 	private final static int SLOTIDX = 0;
 	private final static int TIMEOUTMS = 0;
