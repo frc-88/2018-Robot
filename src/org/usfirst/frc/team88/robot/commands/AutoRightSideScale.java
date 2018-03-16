e28f8e52d101dd9b04f4cb722e7e8a3d3391b8df
tj2 <navtges@gmail.com>
2018-03-15 21:00:51 -0400

    Near side 2 cube???

--- src/org/usfirst/frc/team88/robot/commands/AutoRightSideScale.java
+++ src/org/usfirst/frc/team88/robot/commands/AutoRightSideScale.java
@@ -10,19 +10,31 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoRightSideScale extends CommandGroup {
 
 	public AutoRightSideScale() {
+		// part 1
 		addSequential(new DriveZeroYaw());
 		addParallel(new IntakePneumaticsDown());
-		addParallel(new LiftGotoPosition(Lift.POS_ALMOST_BOTTOM));
+		addParallel(new LiftGotoPosition(Lift.POS_HI_SCALE));
 		addSequential(new AutoDriveDistanceAngleFast("RightScaleDist", "RightScaleAngle_1"));
 		addSequential(new DriveRotateToAngle("RightScaleAngle_2"));
 
-		addSequential(new LiftGotoPosition(Lift.POS_HI_SCALE));
-		addSequential(new LiftCheckOnTarget(Lift.POS_HI_SCALE));
 		addSequential(new IntakeEjectCube(Lift.POS_HI_SCALE));
 
 		addParallel(new LiftSoftLanding());
-		addSequential(new AutoDriveDistance(-10));
 		addSequential(new DriveRotateToAngle("RightScaleAngle_3"));
 		addSequential(new DriveRotateToAngle("RightScaleAngle_3"));
+
+		// part 2
+		addParallel(new AutoDriveDistanceAngleFast("RightScaleDist_2","RightScaleAngle_3"));
+		addSequential(new IntakeIntakeCube(4));
+		addParallel(new LiftGotoPosition(Lift.POS_HI_SCALE));
+		addSequential(new AutoDriveDistanceAngleFast("RightScaleDist_3","RightScaleAngle_3"));
+		addSequential(new DriveRotateToAngle("RightScaleAngle_2"));
+
+		addSequential(new IntakeEjectCube(Lift.POS_HI_SCALE));
+		
+		addParallel(new LiftSoftLanding());
+		addSequential(new DriveRotateToAngle(0));
+		addSequential(new DriveRotateToAngle(0));
+
 	}
 }
