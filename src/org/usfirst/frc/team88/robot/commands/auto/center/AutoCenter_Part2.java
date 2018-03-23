af5ef3d6a76970e59aad24dd4c50160fb94a2fca
Nick Avtges <navtges@gmail.com>
2018-03-23 11:58:14 -0400

    Clean up new center autos

--- src/org/usfirst/frc/team88/robot/commands/auto/center/AutoCenter_Part2.java
+++ src/org/usfirst/frc/team88/robot/commands/auto/center/AutoCenter_Part2.java
@@ -2,7 +2,6 @@ package org.usfirst.frc.team88.robot.commands.auto.center;
 
 import org.usfirst.frc.team88.robot.commands.DriveRotateToAngle;
 import org.usfirst.frc.team88.robot.commands.IntakeIntakeCube;
-import org.usfirst.frc.team88.robot.commands.LiftSoftLanding;
 import org.usfirst.frc.team88.robot.commands.PowerUpConditionalCommand;
 import org.usfirst.frc.team88.robot.commands.auto.AutoDriveDistance;
 
