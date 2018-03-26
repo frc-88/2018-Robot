3518bf67634d074187a493e9a6c0cb0bb06847ea
Nick Avtges <navtges@gmail.com>
2018-03-26 09:56:00 -0400

    Fix the turbo mode bug

--- src/org/usfirst/frc/team88/robot/subsystems/Drive.java
+++ src/org/usfirst/frc/team88/robot/subsystems/Drive.java
@@ -357,9 +357,9 @@ public class Drive extends Subsystem implements PIDOutput {
 	
 	public void enableTURBOMODE(){
 		leftMaster.configPeakOutputForward(1, TIMEOUTMS);
-		leftMaster.configPeakOutputReverse(1, TIMEOUTMS);
+		leftMaster.configPeakOutputReverse(-1, TIMEOUTMS);
 		rightMaster.configPeakOutputForward(1, TIMEOUTMS);
-		rightMaster.configPeakOutputReverse(1, TIMEOUTMS);
+		rightMaster.configPeakOutputReverse(-1, TIMEOUTMS);
 	}
 	
 	public void disableTURBOMODE(){
