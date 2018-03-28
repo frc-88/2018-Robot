23a95d61877270c04293cf3d4109904228806634
Nick Avtges <navtges@gmail.com>
2018-03-28 11:49:40 -0400

    Clean up AutoCenter, adding comments
    Also, only turn towards the scale if we successfully picked up a cube

--- src/org/usfirst/frc/team88/robot/commands/auto/center/AutoCenter.java
+++ src/org/usfirst/frc/team88/robot/commands/auto/center/AutoCenter.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team88.robot.commands.auto.center;
 
+import org.usfirst.frc.team88.robot.commands.HaveCubeConditionalCommand;
 import org.usfirst.frc.team88.robot.commands.StepConditionalCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -10,8 +11,17 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoCenter extends CommandGroup {
 
 	public AutoCenter() {
+		// step 1:
+		//    score preloaded cube into the correct switch
+		//    back off and soft land
+		//    turn towards a second cube
+		//
+		// step 2:
+		//    go get the second cube
+		//    if we have a cube, back off and turn towards our scale
+
 		addSequential(new StepConditionalCommand(new AutoCenter_Part1(), "AutoCenterStep", 1));
 		addSequential(new StepConditionalCommand(new AutoCenter_Part2(), "AutoCenterStep", 2));
-
+		addSequential(new StepConditionalCommand(new HaveCubeConditionalCommand(new AutoCenter_Part3()), "AutoCenterStep", 2));
 	}
 }
