2cc2271356e836dbedd965386ed2af3636ebf56b
Nick Avtges <navtges@gmail.com>
2018-04-02 16:42:39 -0400

    Change AutoCenter to use HaveCubeConditionalCommand

--- src/org/usfirst/frc/team88/robot/commands/auto/center/AutoCenter.java
+++ src/org/usfirst/frc/team88/robot/commands/auto/center/AutoCenter.java
@@ -1,5 +1,6 @@
 package org.usfirst.frc.team88.robot.commands.auto.center;
 
+import org.usfirst.frc.team88.robot.commands.HaveCubeConditionalCommand;
 import org.usfirst.frc.team88.robot.commands.StepConditionalCommand;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
@@ -12,6 +13,6 @@ public class AutoCenter extends CommandGroup {
 	public AutoCenter() {
 		addSequential(new StepConditionalCommand(new AutoCenter_Part1(), "AutoCenterStep", 1));
 		addSequential(new StepConditionalCommand(new AutoCenter_Part2(), "AutoCenterStep", 2));
-
+		addSequential(new StepConditionalCommand(new HaveCubeConditionalCommand(new AutoCenter_Part3()), "AutoCenterStep", 2));
 	}
 }
